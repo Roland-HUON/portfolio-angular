6d8c0d30bffa12b49cba964313a1c294b54f2ffe
Roland HUON <huonroland91@gmail.com>
2024-02-20 08:47:31 +0100

    ✨ path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'skills', component: SkillsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '**', component: PageNotFundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
