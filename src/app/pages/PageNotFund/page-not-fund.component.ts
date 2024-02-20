bf0dfa8a56c056dd840a14dcfab84b97db70b16c
Roland HUON <huonroland91@gmail.com>
2024-02-20 22:12:45 +0100

    ✨ add : add files

---
+++ src/app/pages/PageNotFund/page-not-fund.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './page-not-fund.component.html',
+})
+export class PageNotFundComponent{}
---
+++ src/app/pages/skills/skills.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './skills.component.html',
+})
+export class SkillsComponent{}
